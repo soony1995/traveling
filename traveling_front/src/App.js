5b64eb59160f7f4096ccd779b1b4be44ab07c1ac
soony1995 <sanbonpeople@daum.net>
2022-10-03 10:58:01 +0000

    selenium,db installed

--- traveling_front/src/App.js
+++ traveling_front/src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
 
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const { Builder } = require('selenium-webdriver')
 
-(async function example() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build();
-  try {
-    await driver.get('https://www.google.com');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-  } finally {
-    await driver.quit();
-  }
-})();
+async function start() {
+  const chrome = require('selenium-webdriver/chrome')
+  const options = new chrome.Options()
+
+  options.addArguments('--disable-dev-shm-usage')
+  options.addArguments('--no-sandbox')
+
+  const driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build()
+
+  await driver.get('https://google.com')
+  await driver.sleep(1000)
+  const text = await driver.executeScript('return document.documentElement.innerText')
+  console.log(text)
+  driver.quit()
+}
+
+start()
+
 function App() {
   return (<div>
-        run();
+        
   </div>
   );
 }
---
+++ traveling_front/src/rollOutExperiment.js
@@ -0,0 +1,22 @@
+const { Builder } = require('selenium-webdriver')
+
+async function start() {
+  const chrome = require('selenium-webdriver/chrome')
+  const options = new chrome.Options()
+
+  options.addArguments('--disable-dev-shm-usage') //디렉토리를 사용하지 않는다는 의미이다. 이 디렉토리는 공유 메모리를 담당하는 부분이다
+  options.addArguments('--no-sandbox')
+
+  const driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build()
+
+  await driver.get('https://google.com')
+  await driver.sleep(1000)
+  const text = await driver.executeScript('return document.documentElement.innerText')
+  console.log(text)
+  driver.quit()
+}
+
+start()
